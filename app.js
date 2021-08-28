76aefa4c7a5ac893d96476da2678a07850d493bc
Rohan Dhimal <drowhann@gmail.com>
2021-08-28 14:28:18 +0545

    added some css

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 let text_buttons = document.getElementsByClassName("text__button");
 let result = document.getElementById("score__text");
 
-const findResult = (text) => {
-  console.log(text);
-};
-
 const textButtonClicked = (e) => {
   let text = e.srcElement.innerHTML;
 
   if (text === "C") {
     result.innerHTML = "0";
+  } else if (text === "X") {
+    result.innerHTML += "*";
   } else if (text === "=") {
-    result.innerHTML = findResult(result.innerHTML);
+    result.innerHTML = eval(result.innerHTML);
   } else if (result.innerHTML !== "0" || text === ".") {
     result.innerHTML += text;
   } else {
